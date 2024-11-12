7c47c77521e7d8c76d70a179ad9e998a3d0c5749
ishitasinha8 <ishisinha8@outlook.com>
2024-11-12 23:51:43 +0530

    edit2master

--- ishitasinha8-github-io/src/app/app.routes.ts
+++ ishitasinha8-github-io/src/app/app.routes.ts
@@ -5,10 +5,10 @@ import { TravelComponent } from './travel/travel.component';
 import { WorkComponent } from './work/work.component';
 
 export const routes: Routes = [
-  {path: "", component: HomeComponent},
-  {path: "projects", component: PublicationsComponent},
-  {path: "travel", component:TravelComponent},
-  {path: "profile", component:WorkComponent},
+  {path: "", component: HomeComponent, title: "Ishita Sinha"},
+  {path: "projects", component: PublicationsComponent, title: "Ishita Sinha - Projects"},
+  {path: "photos", component:TravelComponent, title: "Ishita Sinha - Photos"},
+  {path: "profile", component:WorkComponent, title: "Ishita Sinha - Profile"},
   // {path: '**', component: PageNotFoundComponent}
 ];
 
